628ac4e99ae8659341f6757d1aac6442cbc63d88
satyamshinde-dev <150026953+satyamshinde-dev@users.noreply.github.com>
2023-11-14 12:37:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+// var likesSpan = document.querySelectorAll(".likes span");
+// //   console.log(likesSpan[0].innerHTML);
+
+//   var LikesCount = document.querySelectorAll("#LikesCount");
+// //   console.log(LikesCount[0].value);
+
+//   LikesCount[0].addEventListener("change", function LikesCountF(params) {
+   
+// if (value = Number) {
+//     likesSpan[0].innerHTML =LikesCount[0].value 
+// } else {
+//     alert("Enter Likes ")
+// }
+//   })
+
+// var postlike = document.querySelectorAll("#postlike")[0]
+// console.log(postlike);
